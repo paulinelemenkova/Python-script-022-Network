5f4de4076a91e4dd98df7c0071dcf2d184bb276d
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-20 10:39:19 +0800

    Add files via upload

--- Script-022-Network.py
+++ Script-022-Network.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[31]:
-
-
-# libraries
-import pandas as pd
 import numpy as np
+import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
- 
-# Build a dataframe with your connections
-df = pd.DataFrame({ 'from':['Statistical Analysis', 'Visualization', 'Modelling','Statistical Analysis',   ], 
-'to':['Geospatial \nData', 'Statistical Analysis', 'Plotting', 'Modelling', ]})
+
+# build a dataframe with connections
+df = pd.DataFrame({'from':['Statistical Analysis', 'Visualization',
+                           'Modelling','Statistical Analysis'],
+                  'to':['Geospatial \nData', 'Statistical Analysis',
+                        'Plotting', 'Modelling']
+                  }
+                  )
 df
- 
-# Build your graph
-G = nx.DiGraph()   # or DiGraph, 
+
+# build a graph
+G = nx.DiGraph()   # or DiGraph,
 G = nx.from_pandas_edgelist(df, 'from', 'to')
 pos = nx.spring_layout(G)
 
-    
-# Graph with Custom nodes:
-nx.draw(G, with_labels=True, node_size=2000, node_color="#ffd900", node_shape="s", alpha=0.5, linewidths=30)
+# plotting graph with custom nodes:
+nx.draw(G, with_labels=True, node_size=2000,
+        node_color="#ee827c", node_shape="s",
+        alpha=0.5, linewidths=30
+        )
 
 plt.title("Workflow Network of Geospatial Analysis")
-plt.show()
 
+# visualize
+plt.subplots_adjust(top=0.85, bottom=0.08,
+                    left=0.10, right=0.95,
+                    hspace=0.25, wspace=0.35
+                    )
+plt.savefig('plot_Netw.png', dpi=300)
+plt.show()
